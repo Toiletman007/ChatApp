868a04d7342a391beb95fed8f2e9ee081946e028
Toiletman007-PC <quertical5@gmail.com>
2024-05-27 12:30:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.chatapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.chatapp.ui.theme.ChatAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ChatAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Text(text = "test", modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
