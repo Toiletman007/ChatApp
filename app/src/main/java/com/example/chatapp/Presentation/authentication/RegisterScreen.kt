45ee4ccc65771a6343e1d26746e0d6509533c17a
Toiletman007-PC <quertical5@gmail.com>
2024-05-27 18:41:53 +0200

    Extracted PasswordTextFields and added a button for Registering

--- app/src/main/java/com/example/chatapp/Presentation/authentication/RegisterScreen.kt
+++ app/src/main/java/com/example/chatapp/Presentation/authentication/RegisterScreen.kt
@@ -1,12 +1,22 @@
 package com.example.chatapp.Presentation.authentication
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Done
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,52 +24,69 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.chatapp.Presentation.authentication.components.PasswordTextField
 
 @Composable
 fun RegisterScreen(modifier: Modifier = Modifier) {
     var name by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var password2 by remember { mutableStateOf("") }
+    var radioButtonSwitch by remember { mutableStateOf(false) }
 
-    val gradientColors = listOf(
-        Color.Red,
-        Color.Yellow,
-        Color.Green,
-        Color.Cyan,
-        Color.Blue,
-        Color.Magenta
-    )
 
-    val brush = Brush.linearGradient(
-        colors = gradientColors
-    )
 
-    Column(verticalArrangement = Arrangement.SpaceEvenly, horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth().fillMaxHeight(0.6f)) {
+
+    Column(
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.6f)
+    ) {
         Text(text = "Register", fontSize = 70.sp)
-        Column {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
             OutlinedTextField(
-                value = name, onValueChange = { name = it },label = { Text("Name") }, textStyle = TextStyle(brush = brush)
+                value = name,
+                onValueChange = {
+                    if (it.length <= 20) {
+                        name = it
+                    }
+                },
+                label = { Text("Name") },
             )
-            OutlinedTextField(
-                value = password,
-                onValueChange = { password= it },
-                label = { Text("Password") },
-                visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
-            )
-            OutlinedTextField(
-                value = password2,
-                onValueChange = { password2= it },
-                label = { Text("Confirm Password") },
-                visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
-            )
+            Spacer(modifier = Modifier.height(10.dp))
+
+            PasswordTextField(value = password){ password = it }
+            PasswordTextField(value = password2){ password2 = it }
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+
+                RadioButton(selected = radioButtonSwitch, onClick = { radioButtonSwitch = !radioButtonSwitch })
+                TextButton(onClick = { /*TODO*/ }) {
+                    Text(text = "I agree to the terms of service")
+                }
+
+
+            }
+
+            Button(onClick = { if (radioButtonSwitch){
+                //register account
+                Log.d("RegisterScreen: ", "Yes ")
+                Log.d("Register Password", password)
+                Log.d("Register Password", password2)
+            }else{
+                Log.d("RegisterScreen: ", "No ")
+            }
+            }) {
+                Text(text = "Register")
+                Spacer(modifier = modifier.width(10.dp))
+                Icon(imageVector = Icons.Outlined.Done, contentDescription = "Done")
+
+            }
+
         }
 
 
