3d4e8c3100fc6f3ae0a4717fbaeab6115661319e
Toiletman007-PC <quertical5@gmail.com>
2024-05-27 20:30:09 +0200

    Changed colors and reveal password iconbutton

--- app/src/main/java/com/example/chatapp/Presentation/authentication/components/PasswordTextField.kt
+++ app/src/main/java/com/example/chatapp/Presentation/authentication/components/PasswordTextField.kt
@@ -2,30 +2,52 @@ package com.example.chatapp.Presentation.authentication.components
 
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import com.example.chatapp.Presentation.ui.theme.DarkGreen
 
 @Composable
-fun PasswordTextField(modifier: Modifier = Modifier, value:String, onValueChange: (String) -> Unit) {
-
+fun PasswordTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    var passwordVisible by rememberSaveable { mutableStateOf(false) }
     val gradientColors = listOf(
         Color.Red,
         Color.Yellow,
         Color.Green,
-        Color.Cyan,
-        Color.Blue,
-        Color.Magenta
+        Color.Green,
+        DarkGreen,
+        DarkGreen,
+        DarkGreen,
     )
     val brush = Brush.linearGradient(
-        colors = gradientColors
+        colors =
+            if (passwordVisible) {
+                listOf(Color.Black, Color.Black)
+            } else {
+                gradientColors
+            }
+
     )
     OutlinedTextField(
         modifier = Modifier.width(280.dp),
@@ -36,8 +58,30 @@ fun PasswordTextField(modifier: Modifier = Modifier, value:String, onValueChange
             }
         },
         label = { Text("Password") },
-        visualTransformation = PasswordVisualTransformation(),
+        visualTransformation = if (passwordVisible) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        },
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-        textStyle = TextStyle(brush = brush)
+        textStyle = TextStyle(brush = brush),
+        singleLine = true,
+        trailingIcon = {
+            if (passwordVisible) {
+                IconButton(onClick = { passwordVisible = false }) {
+                    Icon(
+                        imageVector = Icons.Filled.Visibility,
+                        contentDescription = "Show Password",
+                    )
+                }
+            } else {
+                IconButton(onClick = { passwordVisible = true }) {
+                    Icon(
+                        imageVector = Icons.Filled.VisibilityOff,
+                        contentDescription = "Hide Password",
+                    )
+                }
+            }
+        }
     )
 }
