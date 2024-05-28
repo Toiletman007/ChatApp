f28a13f3fc21528148a4f9aba04a03cdae20d5c7
Toiletman007-PC <quertical5@gmail.com>
2024-05-29 00:59:03 +0200

    Created

---
+++ app/src/main/java/com/example/chatapp/presentation/navigation/MainNavHost.kt
@@ -0,0 +1,11 @@
+package com.example.chatapp.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun MainNavHost(
+    navController: NavHostController
+) {
+
+}
