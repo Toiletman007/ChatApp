e0c96103d3d13d55abfbde9cdb085971ad816dff
Toiletman007-PC <quertical5@gmail.com>
2024-05-29 01:00:16 +0200

    Made the login Screen

--- app/src/main/java/com/example/chatapp/Presentation/authentication/LoginScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.chatapp.Presentation.authentication
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-@Composable
-fun LoginScreen (modifier: Modifier = Modifier) {
-
-}
---
+++ app/src/main/java/com/example/chatapp/presentation/authentication/LoginScreen.kt
@@ -0,0 +1,73 @@
+package com.example.chatapp.presentation.authentication
+
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.CheckCircle
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.chatapp.presentation.authentication.components.EmailTextField
+import com.example.chatapp.presentation.authentication.components.NameTextField
+import com.example.chatapp.presentation.authentication.components.PasswordTextField
+
+@Composable
+fun LoginScreen (modifier: Modifier = Modifier, onSignInClick: ()-> Unit) {
+    var name by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val radioButtonSwitch by remember { mutableStateOf(false) }
+    var email by remember { mutableStateOf("") }
+
+
+
+    Column(
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.6f)
+    ) {
+        Text(text = "Login", fontSize = 70.sp)
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+            NameTextField(name = name) { name = it }
+            EmailTextField(email = email) { email = it }
+            Spacer(modifier = Modifier.height(15.dp))
+            PasswordTextField(value = password, text = "Password", color = false){ password = it }
+
+            Spacer(modifier = Modifier.height(20.dp))
+            Button(modifier = Modifier, shape = RoundedCornerShape(14.dp)
+                ,onClick = { if (radioButtonSwitch){
+                    //register account
+                    Log.d("RegisterScreen: ", "Yes ")
+                    Log.d("Register Password", password)
+                }else{
+                    //Don't
+                    Log.d("RegisterScreen: ", "No ")
+                }
+                }) {
+                Text(text = "Login")
+                Spacer(modifier = modifier.width(15.dp))
+                Icon(imageVector = Icons.Outlined.CheckCircle, contentDescription = "Done")
+
+            }
+
+        }
+    }
+}
