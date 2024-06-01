ca45344fef2c9a79dd7d93d8d0acda725af65779
Toiletman007-PC <quertical5@gmail.com>
2024-06-02 00:18:15 +0200

    fixed

--- app/src/main/java/com/example/chatapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/presentation/MainActivity.kt
@@ -6,7 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
 import com.example.chatapp.presentation.navigation.MainNavHost
 import com.example.compose.ChatAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
