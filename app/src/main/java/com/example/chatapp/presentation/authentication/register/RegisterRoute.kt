148876f0aaad63363fe35b0533f5ee48bc329c51
Toiletman007-PC <quertical5@gmail.com>
2024-06-02 00:16:48 +0200

    fixed

--- app/src/main/java/com/example/chatapp/presentation/authentication/register/RegisterRoute.kt
+++ app/src/main/java/com/example/chatapp/presentation/authentication/register/RegisterRoute.kt
@@ -1,5 +1,6 @@
 package com.example.chatapp.presentation.authentication.register
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
@@ -7,13 +8,16 @@ import com.example.chatapp.presentation.util.NavControllerHolder
 
 @Composable
 fun RegisterRoute(
-    coordinator: RegisterCoordinator = rememberRegisterCoordinator(viewModel = RegisterViewModel(navControllerHolder = NavControllerHolder()))
+    coordinator: RegisterCoordinator =
+        rememberRegisterCoordinator(
+            viewModel = RegisterViewModel(
+                navControllerHolder = NavControllerHolder()))
 ) {
     // State observing and declarations
-    val uiState by coordinator.screenStateFlow
+    val uiState: RegisterState by coordinator.screenStateFlow
 
     // UI Actions
-    val actions = rememberRegisterActions()
+    val actions = rememberRegisterActions(coordinator)
 
     // UI Rendering
     RegisterScreen(state = uiState, actions = actions)
@@ -21,9 +25,30 @@ fun RegisterRoute(
 
 
 @Composable
-fun rememberRegisterActions(): RegisterActions {
-    return remember() {
+fun rememberRegisterActions(coordinator: RegisterCoordinator): RegisterActions {
+    return remember(coordinator) {
         RegisterActions(
+            onRegister = {
+                         Log.d("Test2", "Register acc")
+//                Validate data and register account
+            },
+            onNameChange = { newName ->
+                coordinator.onChangeName(name = newName)
+            },
+            onEmailChange = { newEmail ->
+                coordinator.onChangeEmail(email = newEmail)
+            },
+            onPasswordChange = {newPassword ->
+                coordinator.onChangePassword(password = newPassword)
+            },
+            onPassword2Change = {newPassword2 ->
+                coordinator.onChangePassword2(password2 = newPassword2)
+            },
+            onCheckBoxSwitchChange = {newCheckBoxSwitchState ->
+                coordinator.onCheckBoxSwitchChange(checkBoxState = newCheckBoxSwitchState)
+            }
+
+
 
         )
     }
